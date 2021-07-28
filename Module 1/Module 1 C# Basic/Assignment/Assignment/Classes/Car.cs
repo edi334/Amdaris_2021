35fb9228731da5a22bf7ec06f8fb83d4f8b00e22
Eduard Cziszter <eduard.cziszter@amdaris.com>
2021-07-28 12:48:03 +0300

    assigment done

---
+++ Module 1/Module 1 C# Basic/Assignment/Assignment/Classes/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Classes
+{
+    class Car
+    {
+        static Car()
+        {
+            CarCount = 1;
+            Console.WriteLine("Static constructor says there is already a car in the garage!");
+        }
+
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public void Write()
+        {
+            Console.WriteLine("The car is a " + Brand + " " + Name + ".");
+        }
+        public void CheckNameNoRef(string name, bool areSame)
+        {
+            areSame = Name == name;
+        }
+        public void CheckNameRef(string name, ref bool areSame)
+        {
+            areSame = Name == name;
+        }
+        public void CheckNameOut(string name, out bool areSame)
+        {
+            areSame = Name == name;
+        }
+        public void CheckNameRefType (string name, bool? areSameRef)
+        {
+            areSameRef = Name == name;
+        }
+        public static int CarCount { get; set; }
+        public static void StartCar()
+        {
+            Console.WriteLine("Starting engine!");
+        }
+        public static void Prepare()
+        {
+            Console.WriteLine("Preparing garage!");
+        }
+    }
+}
--- Module 1/Module 1 C# Basic/Assignment/Assignment/Program.cs
+++ Module 1/Module 1 C# Basic/Assignment/Assignment/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment.Classes;
+using System;
 
 namespace Assignment
 {
@@ -7,6 +8,50 @@ namespace Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Car.Prepare();
+
+            Car c1 = new Car { Name = "Rapid", Brand = "Skoda" };
+            Car.StartCar();
+            Car.CarCount++;
+            c1.Write();
+
+            Car c2 = new Car { Name = "Passat", Brand = "Volkswagen" };
+            Car.StartCar();
+            Car.CarCount++;
+            c2.Write();
+
+            Console.WriteLine("There are " + Car.CarCount + " cars!");
+
+            string name = "Octavia";
+            bool areSame = true;
+            bool areSameOut;
+
+            //It's still true because the argument is passed by value
+            c1.CheckNameNoRef(name, areSame);
+            Console.WriteLine(areSame);
+
+            //It's false because the argument is passed by reference
+            c1.CheckNameRef(name, ref areSame);
+            Console.WriteLine(areSame);
+
+            //It's false because the argument is passed by reference
+            c1.CheckNameOut(name, out areSameOut);
+            Console.WriteLine(areSameOut);
+
+            //It's false even though the parameter is not passed by value because areSameRef is a reference-type 
+            bool? areSameRef = areSame;
+            c1.CheckNameRefType(name, areSameRef);
+            Console.WriteLine(areSameRef);
+
+            areSameRef = null;
+            //This is true
+            Console.WriteLine(areSameRef == null);
+            areSameRef = true;
+
+            areSame = (bool)areSameRef;
+            c1.CheckNameNoRef(name, areSame);
+            //It's still true because areSame is a Value-Type
+            Console.WriteLine(areSame);
         }
     }
 }
