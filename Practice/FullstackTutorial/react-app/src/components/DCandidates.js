9ebf88f2e11d74b3e2865a79a0b3189220d6ebd9
Eduard Cziszter <eduard.cziszter@amdaris.com>
2021-07-29 16:20:50 +0300

    react tutorial progress

--- Practice/FullstackTutorial/react-app/src/components/DCandidates.js
+++ Practice/FullstackTutorial/react-app/src/components/DCandidates.js
@@ -1,22 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/dCandidate";
+import { Grid, Paper, TableCell, TableContainer, TableHead, TableRow, Table, TableBody, withStyles } from "@material-ui/core";
+import DCandidateForm from "./DCandidateForm";
 
-const DCandidates = (props) => {
+const styles = theme => ({
+    paper: {
+        margin: theme.spacing(2),
+        padding: theme.spacing(2)
+    }
+})
+
+const DCandidates = ({classes, ...props}) => {
     useEffect(() => {
         props.fetchAllDCandidates()
     }, [])
     return (
-        <div>from DCandidates</div>
+        <Paper className = {classes.paper} elevation = {3}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <DCandidateForm/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Mobile</TableCell>
+                                    <TableCell>Blood Group</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    props.dCandidateList.map((record, index) => {
+                                        return (
+                                            <TableRow key={index} hover>
+                                                <TableCell>{record.fullName}</TableCell>
+                                                <TableCell>{record.phoneNumber}</TableCell>
+                                                <TableCell>{record.bloodGroup}</TableCell>
+                                            </TableRow>
+                                        )
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
 
 const mapStateToProps = state => ({
-    dCandidateList: state.dCandidate.List
+    dCandidateList: state.dCandidate.list
 })
 
 const mapActionToProps = {
     fetchAllDCandidates: actions.fetchAll
 }
 
-export default connect(mapStateToProps, mapActionToProps)(DCandidates);
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(DCandidates));
