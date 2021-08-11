c0fccac3c1a3d4f02586b3c38543782696d8bb8d
Eduard Cziszter <eduard.cziszter@amdaris.com>
2021-08-11 09:33:27 +0300

    assignment done

---
+++ Module 1/Module 1 Design Patterns - Behavioral & IoC and Pragmatic Principles/Assignment/Assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using IContainer = Autofac.IContainer;
+
+namespace Assignment
+{
+    public class Ping : IRequest<string> { }
+    public class Pong : IRequestHandler<Ping, string>
+    {
+        public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult("Request handled");
+        }
+    }
+
+    public class Pinger
+    {
+        private readonly IMediator _mediator;
+
+        public Pinger(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public string SendPing()
+        {
+            return _mediator.Send(new Ping()).Result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceLocator serviceLocator = new ServiceLocator();
+
+            var mediator = serviceLocator.BuildMediator();
+
+            Pinger pinger = new Pinger(mediator);
+
+            Console.WriteLine(pinger.SendPing());
+        }
+    }
+}
---
+++ Module 1/Module 1 Design Patterns - Behavioral & IoC and Pragmatic Principles/Assignment/Assignment/ServiceLocator.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class ServiceLocator
+    {
+        private readonly ServiceCollection services = new ServiceCollection();
+
+        public IMediator BuildMediator()
+        {
+            services.AddMediatR(typeof(Ping));
+            services.AddScoped(typeof(IRequestHandler<Ping,string>), typeof(Pong));
+
+            var provider = services.BuildServiceProvider();
+
+            return provider.GetRequiredService<IMediator>();
+        }
+
+        
+    }
+}
