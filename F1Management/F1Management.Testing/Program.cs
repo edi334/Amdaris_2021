a14818ba5ab80118610e3a70adf32cea6bea079c
Eduard Cziszter <eduard.cziszter@amdaris.com>
2021-08-11 14:24:44 +0300

    made lists for all the mocked objects

--- F1Management/F1Management.Testing/Program.cs
+++ F1Management/F1Management.Testing/Program.cs
@@ -3,7 +3,6 @@ using F1Management.Core.Models;
 using F1Management.Core.Models.Car;
 using F1Management.Core.Models.Roles;
 using F1Management.Core.Models.Roles.Mechanics;
-using System;
 using System.Collections.Generic;
 
 namespace F1Management.Testing
@@ -28,112 +27,111 @@ namespace F1Management.Testing
             };
 
             Race race = new Race
-            { 
+            {
                 Id = "1",
                 Name = "2021 Italian Grand Prix",
                 CircuitName = "Autodromo Internazzionale Monza"
             };
 
+            List<RaceCar> cars = new List<RaceCar>
+            {
+                new RaceCar
+                (
+                    "1",
+                    "1",
+                    new Chassis { FrontWing = "small", RearWing = "small", BodyWork = "Aerodynamical", Wear = 20 },
+                    new Engine { Brand = "Mercedes", HorsePower = 1000, Wear = 15 },
+                    new Gearbox { Wear = 25 },
+                    new List<Tire>
+                    {
+                        new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 15 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 13 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 10 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 14 }
+                    }
+                ),
+                new RaceCar
+                (
+                    "2",
+                    "1",
+                    new Chassis { FrontWing = "small", RearWing = "small", BodyWork = "Aerodynamical", Wear = 30 },
+                    new Engine { Brand = "Mercedes", HorsePower = 1000, Wear = 20 },
+                    new Gearbox { Wear = 40 },
+                    new List<Tire>
+                    {
+                        new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 8 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 5 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 5 },
+                        new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 7 }
+                    }
+                )
+            };
 
-            RaceCar car1 = new RaceCar
-            (
-                "1",
-                "1",
-                new Chassis { FrontWing = "small", RearWing = "small", BodyWork = "Aerodynamical", Wear = 20 },
-                new Engine { Brand = "Mercedes", HorsePower = 1000, Wear = 15 },
-                new Gearbox { Wear = 25 },
-                new List<Tire>
+            List<Role> roles = new List<Role>
+            {
+                new Driver { Id = "1", CarId = "1", Number = 44, Points = 152 },
+                new Driver { Id = "2", CarId = "2", Number = 77, Points = 84 },
+                new TeamPrincipal() { Id = "3" },
+                new RaceEngineer("1") { Id = "4" },
+                new RaceEngineer("2") { Id = "5" },
+                new PitStopMechanic() { Id = "6" },
+                new CarMechanic() { Id = "7" }
+            };
+
+            List<TeamMember> teamMembers = new List<TeamMember>
+            {
+                new TeamMember {
+                    Id = "1",
+                    RoleId = "3",
+                    TeamId = "1",
+                    Name = "Toto Wolff"
+                },
+                new TeamMember
                 {
-                    new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 15 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 13 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 10 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Medium, Wear = 14 }
-                }
-            );
-
-            RaceCar car2 = new RaceCar
-            (
-                "2",
-                "1",
-                new Chassis { FrontWing = "small", RearWing = "small", BodyWork = "Aerodynamical", Wear = 30 },
-                new Engine { Brand = "Mercedes", HorsePower = 1000, Wear = 20 },
-                new Gearbox { Wear = 40 },
-                new List<Tire>
+                    Id = "2",
+                    RoleId = "1",
+                    TeamId = "1",
+                    Name = "Lewis Hamilton"
+                },
+                new TeamMember
                 {
-                    new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 8 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 5 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 5 },
-                    new Tire {Brand = "Pirelli", Type = TireType.Soft, Wear = 7 }
+                    Id = "3",
+                    RoleId = "2",
+                    TeamId = "1",
+                    Name = "Valteri Bottas"
+                },
+
+                new TeamMember
+                {
+                    Id = "4",
+                    RoleId = "4",
+                    TeamId = "1",
+                    Name = "Peter Bonnington"
+                },
+                new TeamMember
+                {
+                    Id = "5",
+                    RoleId = "5",
+                    TeamId = "1",
+                    Name = "Jonathan Eddolls"
+                },
+
+                new TeamMember
+                {
+                    Id = "6",
+                    RoleId = "6",
+                    TeamId = "1",
+                    Name = "Andrew Smith"
+                },
+                new TeamMember
+                {
+                    Id = "7",
+                    RoleId = "7",
+                    TeamId = "1",
+                    Name = "Alexander Arnold"
                 }
-            );
-
-            TeamPrincipal principal = new TeamPrincipal() { Id = "3" };
-
-            Driver driver1 = new Driver { Id = "1", CarId = "1", Number = 44, Points = 152 };
-            Driver driver2 = new Driver { Id = "2", CarId = "2", Number = 77, Points = 84 };
-
-            RaceEngineer engineer1 = new RaceEngineer("1") { Id = "4" };
-            RaceEngineer engineer2 = new RaceEngineer("2") { Id = "5" };
-
-            PitStopMechanic pitStopMechanic = new PitStopMechanic() { Id = "6" };
-            CarMechanic carMechanic = new CarMechanic() { Id = "7" };
-
-            TeamMember toto = new TeamMember
-            {
-                Id = "1",
-                RoleId = "3",
-                TeamId = "1",
-                Name = "Toto Wolff"
-            };
-
-            TeamMember lewis = new TeamMember
-            {
-                Id = "2",
-                RoleId = "1",
-                TeamId = "1",
-                Name = "Lewis Hamilton"
-            };
-
-            TeamMember bottas = new TeamMember
-            {
-                Id = "3",
-                RoleId = "2",
-                TeamId = "1",
-                Name = "Valteri Bottas"
-            };
-
-            TeamMember bono = new TeamMember
-            {
-                Id = "4",
-                RoleId = "4",
-                TeamId = "1",
-                Name = "Peter Bonnington"
-            };
-
-            TeamMember jon = new TeamMember
-            {
-                Id = "5",
-                RoleId = "5",
-                TeamId = "1",
-                Name = "Jonathan Eddolls"
-            };
-
-            TeamMember ps = new TeamMember
-            {
-                Id = "6",
-                RoleId = "6",
-                TeamId = "1",
-                Name = "Andrew Smith"
-            };
-
-            TeamMember cm = new TeamMember
-            {
-                Id = "7",
-                RoleId = "7",
-                TeamId = "1",
-                Name = "Alexander Arnold"
             };
 
         }
     }
-}
+     
